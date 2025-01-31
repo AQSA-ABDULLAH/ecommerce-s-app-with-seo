5ca84cc99516d9b403163044ff28bd465679a69c
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2025-01-31 12:14:55 +0500

    fix issues

--- app/layout.js
+++ app/layout.js
@@ -49,26 +49,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* Open Graph meta tags */}
-        <meta property="og:title" content={metadata.openGraph.title} />
-        <meta property="og:description" content={metadata.openGraph.description} />
-        <meta property="og:image" content={metadata.openGraph.images[0].url} />
-        <meta property="og:url" content={metadata.openGraph.url} />
-        <meta property="og:site_name" content={metadata.openGraph.siteName} />
-        
-        {/* Twitter meta tags */}
-        <meta name="twitter:card" content={metadata.twitter.card} />
-        <meta name="twitter:site" content={metadata.twitter.site} />
-        <meta name="twitter:title" content={metadata.twitter.title} />
-        <meta name="twitter:description" content={metadata.twitter.description} />
-        <meta name="twitter:image" content={metadata.twitter.images[0].url} />
-
-        <meta name="description" content={metadata.description} />
-        <meta name="author" content="AQSA ABDULLAH" />
-      </head>
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#171717] text-[#EDEDED] font-style`}
       >
